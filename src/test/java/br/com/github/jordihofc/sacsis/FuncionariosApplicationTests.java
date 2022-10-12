3a4a54edf3d4b69a37316f4f535995a802163e9e
jordihofc <jordyhenrique7@gmail.com>
2022-10-12 09:08:46 -0300

    pronto para iniciar workshop

---
+++ src/test/java/br/com/github/jordihofc/sacsis/FuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.jordihofc.sacsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
